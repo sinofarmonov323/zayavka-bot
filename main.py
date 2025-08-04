e4ed6ff7ce54746636546cdb9b7506a52491b318
sinofarmonov323 <farmonovsino4@gmail.com>
2025-08-04 15:53:12 +0500

    updated

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ async def start(message: types.Message):
 @dp.chat_join_request()
 async def handle_chat_join_request(event: types.ChatJoinRequest, bot: Bot):   
     add_user(event.from_user.id, event.from_user.username) 
-    await bot.send_photo(event.from_user.id, photo=types.FSInputFile("image.png"), caption="𝗔𝘀𝘀𝗮𝗹𝗼𝗺𝘂 𝗮𝗹𝗮𝘆𝗸𝘂𝗺😊\n\n𝗦𝗶𝘇 𝗩𝗶𝗽 𝗸𝗮𝗻𝗮𝗹 𝘃𝗮 𝗦𝗵𝗼𝗸𝗵 𝗮𝗸𝗲𝗻𝗶𝗻𝗴 𝟯𝟬𝟬𝟬$ 𝗹𝗶𝗸 𝗸𝘂𝗿𝘀𝗶𝗻𝗶 𝘆𝘂𝘁𝗶𝗯 𝗼𝗹𝗱𝗶𝗻𝗴𝗶𝘇😎\n\n𝗨𝗹𝗮𝗿𝗻𝗶 𝗾𝗼𝗹𝗴𝗮 𝗸𝗶𝗿𝗶𝘁𝗶𝘀𝗵 𝘂𝗰𝗵𝘂𝗻 𝗽𝗮𝘀𝗱𝗮𝗴𝗶 𝗧𝘂𝗴𝗺𝗮𝗹𝗮𝗿𝗱𝗮𝗻 𝗳𝗼𝘆𝗱𝗮𝗹𝗮𝗻𝗶𝗻𝗴👇", reply_markup=(send_buttons("https://t.me/+ZeVl1BrIZWo0NmEy", "https://t.me/+ki3LHeP7FStlMmUy")))
-    # await event.approve() # bu kod foydalanuvchi zayavka tashalasa avtomatik qabul qiladi1
+    chat_invite_link1 = await bot.create_chat_invite_link(-1002533324913)
+    chat_invite_link2 = await bot.create_chat_invite_link(-1002831908480)
+    await bot.send_photo(event.from_user.id, photo=types.FSInputFile("image.png"), caption="𝗔𝘀𝘀𝗮𝗹𝗼𝗺𝘂 𝗮𝗹𝗮𝘆𝗸𝘂𝗺😊\n\n𝗦𝗶𝘇 𝗩𝗶𝗽 𝗸𝗮𝗻𝗮𝗹 𝘃𝗮 𝗦𝗵𝗼𝗸𝗵 𝗮𝗸𝗲𝗻𝗶𝗻𝗴 𝟯𝟬𝟬𝟬$ 𝗹𝗶𝗸 𝗸𝘂𝗿𝘀𝗶𝗻𝗶 𝘆𝘂𝘁𝗶𝗯 𝗼𝗹𝗱𝗶𝗻𝗴𝗶𝘇😎\n\n𝗨𝗹𝗮𝗿𝗻𝗶 𝗾𝗼𝗹𝗴𝗮 𝗸𝗶𝗿𝗶𝘁𝗶𝘀𝗵 𝘂𝗰𝗵𝘂𝗻 𝗽𝗮𝘀𝗱𝗮𝗴𝗶 𝗧𝘂𝗴𝗺𝗮𝗹𝗮𝗿𝗱𝗮𝗻 𝗳𝗼𝘆𝗱𝗮𝗹𝗮𝗻𝗶𝗻𝗴👇", reply_markup=(send_buttons(chat_invite_link1, chat_invite_link2)))
+    # await event.approve() # bu kod foydalanuvchi zayavka tashalasa avtomatik qabul qiladi
 
 @dp.message(and_f(F.chat.id == ADMIN_ID, F.text.startswith("/admin")))
 async def admin_command_handler(message: types.Message):
